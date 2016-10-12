2f3409b325777e634887ac0f39603ac4b71e64b1
Brad <bcb1771@gmail.com>
2016-10-11 19:06:07 -0600

    1.41

---
+++ New Unity Project 3/Assets/Prefab/Terrain/DestroyGamePieces.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyGamePieces : MonoBehaviour {
+	//public float gravityForces = - 20;
+
+
+	void OnCollisionExit (Collision col)
+	{
+		print ("test");
+		if (col.gameObject.tag == "boundry") {
+			Destroy (gameObject);
+		} /*else {
+			
+			Physics.gravity = new Vector3 (0,gravityForces, 0 );
+		}*/
+			
+	}
+}
---
+++ New Unity Project 3/Assets/scripts/MoveBG.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBG : MonoBehaviour {
+	public float Speed = - 0.2f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector3 (Speed, 0, 0) * Time.deltaTime);
+	}
+}
